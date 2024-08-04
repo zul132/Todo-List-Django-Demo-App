16ac368f5e40646ca2cff51650a066a0cc8d85e4
zul132 <fathimazulaikha.25cs@licet.ac.in>
2024-08-04 18:40:06 +0530

    Added HTML templates

--- demo/myapp/views.py
+++ demo/myapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here. Views can be created for different routes that we can access on our website
 def home(request):
-    return HttpResponse("Hello World!")
+    return render(request, "home.html")
